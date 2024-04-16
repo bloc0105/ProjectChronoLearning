0c49a210dd90faec129354796fd9abbad5fd52c5
bloc0105 <30642511+bloc0105@users.noreply.github.com>
2024-04-15 21:59:10 -0500

    Update coordinates.cpp for Rotations

--- coordinates/coordinates.cpp
+++ coordinates/coordinates.cpp
@@ -12,5 +12,15 @@ int main()
     std::cout << "y = " << somevector[1] << "\n";
     std::cout << "z = " << somevector[2] << "\n";
 
+    chrono::ChQuaterniond rotator = chrono::QuatFromAngleY(30 * chrono::CH_DEG_TO_RAD);
+
+    chrono::ChVector3d rot = rotator.Rotate(somevector);
+
+    std::cout << "_________________________________________________________________\n\n";
+
+    std::cout << "x = " << rot[0] << "\n";
+    std::cout << "y = " << rot[1] << "\n";
+    std::cout << "z = " << rot[2] << "\n";
+
     return 0;
 }
