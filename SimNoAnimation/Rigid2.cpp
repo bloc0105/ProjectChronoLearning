3e53662f1954285e0ef78c7787a52f9fbb0987f4
Andy Block <andrewblock30@gmail.com>
2024-05-02 03:52:33 +0000

    Added A rigid Body

--- SimNoAnimation/Rigid2.cpp
+++ SimNoAnimation/Rigid2.cpp
@@ -14,17 +14,31 @@ int main()
     double rot_angle = 50.0 * chrono::CH_DEG_TO_RAD;
     auto the_body = std::make_shared<chrono::ChBody>();
 
+    chrono::ChCoordsys pos_coodinates(chrono::ChVector3d(0, 0, 0), chrono::ChQuaterniond(1, 0, 0, 0));
 
-    chrono::ChFramed box_frame(chrono::ChVector3d(0, 0, 0),chrono::ChQuaterniond(1,0,0,0));
+    chrono::ChCoordsys vel_coodinates(chrono::ChVector3d(1,.22222,0.3),chrono::ChQuaterniond(1,0,0,0));
+    chrono::ChCoordsys acc_coodinates(chrono::ChVector3d(0,0.25,0),chrono::ChQuaterniond(1,0,0,0));
 
-    // Set the physical properties of the box.
-    the_body->SetCoordsys(chrono::ChCoordsys(box_frame.GetCoordsys()));
-    the_body->SetAngVelLocal(chrono::ChVector3d(rot_angle,0,0));
+    chrono::ChFramed box_frame(pos_coodinates);
+    the_body->SetCoordsys(box_frame.GetCoordsys());
+    // the_body->SetLinVel(chrono::ChVector3d(0, 0, 0));
+    // the_body->SetLinAcc(chrono::ChVector3d(0, 0.25, 0));
+    the_body->SetCoordsysDt(vel_coodinates);
+    the_body->SetCoordsysDt2(acc_coodinates);
 
+    chrono::ChSystemNSC sys;
 
-    std::cout << "Rotation angle should be " << rot_angle << std::endl;
-    std::cout << "Prior angular Velocity is " << the_body->GetAngVelLocal() << std::endl;
-    std::cout << "The Ratio is " << rot_angle / (the_body->GetAngVelLocal()[0]) << std::endl;
+    sys.SetGravitationalAcceleration(0.0);
+
+    sys.AddBody(the_body);
+
+    double frame_time_interval = 0.1;
+
+    for (double frame_time = 0.0; frame_time < 5.0; frame_time += frame_time_interval)
+    {
+        std::cout << "Position of the Box is " << the_body->GetPos() << std::endl;
+        sys.DoStepDynamics(frame_time_interval);
+    }
 
     return 0;
 }
