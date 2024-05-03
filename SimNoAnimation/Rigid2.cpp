f2c3c027172f8f460d265fbc5e18b56ec42d0900
Andy Block <adblock@mtu.edu>
2024-05-03 01:38:58 +0000

    Removed Extra Lines

--- SimNoAnimation/Rigid2.cpp
+++ SimNoAnimation/Rigid2.cpp
@@ -13,22 +13,19 @@ int main()
 
     double rot_angle = 50.0 * chrono::CH_DEG_TO_RAD;
     auto the_body = std::make_shared<chrono::ChBody>();
+    the_body->SetMass(1);
+
 
     chrono::ChCoordsys pos_coodinates(chrono::ChVector3d(0, 0, 0), chrono::ChQuaterniond(1, 0, 0, 0));
 
-    chrono::ChCoordsys vel_coodinates(chrono::ChVector3d(1,.22222,0.3),chrono::ChQuaterniond(1,0,0,0));
-    chrono::ChCoordsys acc_coodinates(chrono::ChVector3d(0,0.25,0),chrono::ChQuaterniond(1,0,0,0));
-
+    
     chrono::ChFramed box_frame(pos_coodinates);
-    the_body->SetCoordsys(box_frame.GetCoordsys());
-    // the_body->SetLinVel(chrono::ChVector3d(0, 0, 0));
-    // the_body->SetLinAcc(chrono::ChVector3d(0, 0.25, 0));
-    the_body->SetCoordsysDt(vel_coodinates);
-    the_body->SetCoordsysDt2(acc_coodinates);
+    // the_body->SetLinVel(chrono::ChVector3d(1, 0, 0));
+    the_body->SetLinAcc(chrono::ChVector3d(1, 0, 0));
 
     chrono::ChSystemNSC sys;
 
-    sys.SetGravitationalAcceleration(0.0);
+    sys.SetGravitationalAcceleration(chrono::ChVector3d(1,0,0));
 
     sys.AddBody(the_body);
 
