7657239ae3ac0ff7ac10070a31f0006b6c6374ca
Andy Block <andrewblock30@gmail.com>
2024-05-07 02:12:41 +0000

    Printlines to indicate

--- SimNoAnimation/Rigid2.cpp
+++ SimNoAnimation/Rigid2.cpp
@@ -15,27 +15,30 @@ int main()
     auto the_body = std::make_shared<chrono::ChBody>();
     the_body->SetMass(1);
 
-
     chrono::ChCoordsys pos_coodinates(chrono::ChVector3d(0, 0, 0), chrono::ChQuaterniond(1, 0, 0, 0));
 
-    
     chrono::ChFramed box_frame(pos_coodinates);
     // the_body->SetLinVel(chrono::ChVector3d(1, 0, 0));
     the_body->SetLinAcc(chrono::ChVector3d(1, 0, 0));
+    std::cout << "Initial acceleration set to " << the_body->GetLinAcc() << std::endl;
 
     chrono::ChSystemNSC sys;
+    std::cout << "After making system, acceleration set to " << the_body->GetLinAcc() << std::endl;
 
-    sys.SetGravitationalAcceleration(chrono::ChVector3d(1,0,0));
+    sys.SetGravitationalAcceleration(chrono::ChVector3d(0, 0, 0));
+    std::cout << "After setting Gravity, acceleration set to " << the_body->GetLinAcc() << std::endl;
 
     sys.AddBody(the_body);
+    std::cout << "After Adding Body, acceleration set to " << the_body->GetLinAcc() << std::endl;
 
     double frame_time_interval = 0.1;
 
     for (double frame_time = 0.0; frame_time < 5.0; frame_time += frame_time_interval)
     {
-        std::cout << "Position of the Box is " << the_body->GetPos() << std::endl;
+        std::cout << "Pos: " << the_body->GetPos()
+                  << " ___ Vel: " << the_body->GetLinVel() << " ___ Acc: " << the_body->GetLinAcc() << std::endl;
         sys.DoStepDynamics(frame_time_interval);
     }
-
+    std::cout << "The end, acceleration set to " << the_body->GetLinAcc() << std::endl;
     return 0;
 }
